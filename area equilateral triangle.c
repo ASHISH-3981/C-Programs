7c42ac52561e6b85d460a9efafece6a4f563a752
ASHISH KUMAR SINHA <82906808+ASHISH-3981@users.noreply.github.com>
2022-10-26 18:51:11 +0530

    Add files via upload

---
+++ area equilateral triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+float side , area ;
+printf ("enter the side= ");
+scanf  ("%f",&side);
+
+area=(1.73/4)*side*side;
+printf ("area of equilateral triangle =%f",area);
+
+}
---
+++ area of cubiod .c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main ()
+{int b,h,l,area;
+printf ("enter base,  height and  length");
+scanf ("%d%d%d",&b,&h,&l);
+ area= b*h*l;
+printf ("area of cuboid %d",area );
+}

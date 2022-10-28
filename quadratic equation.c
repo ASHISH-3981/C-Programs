ab54d1638f3ef587b400bbc18b1d0b8d377e5920
ASHISH KUMAR SINHA <82906808+ASHISH-3981@users.noreply.github.com>
2022-10-28 23:10:36 +0530

    Update quadratic equation.c

--- quadratic equation.c
+++ quadratic equation.c
@@ -12,21 +12,27 @@ scanf ("%f%f%f",&a,&b,&c);
 if (discriminant>=0)
 
     {
-   printf ("roots are real and unequal\n ");
-   root1 = ((-b+sqrt(discriminant))/2*a);
-   root2= ((-b-sqrt(discriminant))/2*a);
-    printf ("root1= %f and root2= %f",root1, root2);
-    }
-    else
+        if(discriminant>0)
+        {
+        printf ("roots are real and unequal\n ");
+        root1 = ((-b+sqrt(discriminant))/2*a);
+        root2= ((-b-sqrt(discriminant))/2*a);
+        printf ("root1= %f and root2= %f",root1, root2);
+        }
+       else
         {
         printf ("root are real and equal\n");
+
+        root1=root2=b/(2*a);
+        printf("root1=%f and root2=%f",root1,root2);
         }
-   else
-{
-printf ("root and imaginary");
-real =-b/(2*a);
-imaginary= sqrt (fabs(discriminant))/(2*a);
-printf ("real part %f, imagimary part= %f",real,imaginary);
+    }
+else
+  {
+   printf ("root and imaginary");
+   real =-b/(2*a);
+   imaginary= sqrt (fabs(discriminant))/(2*a);
+   printf ("real part %f, imagimary part= %f",real,imaginary);
 }
 
 }
